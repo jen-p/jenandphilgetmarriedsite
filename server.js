84c60a9d9f40a946d09b7ad58b713b460cceff41
Phil Sobus <phil.sobus@gmail.com>
2018-03-07 11:38:27 -0500

    updating server code

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express'),
+	app = express();
 
 app.use(express.static('./'));
 
-var server = app.listen(1337, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(1337, () => {
+  let host = LOCAL_IP(),
+  	port = server.address().port;
   
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log(`Example app listening at http://${host}:${port} or http://localhost:${port}`);
 });
+
+const LOCAL_IP = () => {
+	let interfaces = require("os").networkInterfaces();
+
+	for(let i in interfaces){
+		for(let k in interfaces[i]){
+			let address = interfaces[i][k];
+
+			if(address.family === "IPv4" && !address.internal){
+				return address.address;
+			}
+		}
+	}
+};
