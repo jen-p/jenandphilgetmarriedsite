42a39a16eb491e16568b737a2faa898a1da5b41b
jen-p <jennifer@goinvo.com>
2018-03-24 19:46:43 -0400

    images
    - switched out headshots
    - added favicon

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const express = require('express'),
 
 app.use(express.static('./'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/mockSubmission", function(req, res) {
 	res.json(req.body);
 });
 
 const server = app.listen(1337, () => {
-  let host = LOCAL_IP(),
+  var host = LOCAL_IP(),
   	port = server.address().port;
-  
+
   console.log(`Example app listening at http://${host}:${port} or http://localhost:${port}`);
 });
 
 const LOCAL_IP = () => {
-	let interfaces = require("os").networkInterfaces();
+	var interfaces = require("os").networkInterfaces();
 
-	for(let i in interfaces){
-		for(let k in interfaces[i]){
-			let address = interfaces[i][k];
+	for(var i in interfaces){
+		for(var k in interfaces[i]){
+			var address = interfaces[i][k];
 
 			if(address.family === "IPv4" && !address.internal){
 				return address.address;
